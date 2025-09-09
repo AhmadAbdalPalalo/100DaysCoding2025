5900b486749d6da76676216d305f4641e799139b
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-09 08:02:03 +0800

    Create Day_2.java

---
+++ Day_2.java
@@ -0,0 +1,18 @@
+package Abdal_100dayscoding;
+
+public class Abdal_100dayscoding {
+public static void main(String[] args) {
+    System.out.print("Saya Abdal");
+        System.out.println("Aku dari tadi");
+        System.out.printf("Nama saya %s, Umur saya %d", "Abdal Ganteng", 18);
+        
+        //print digunakan untuk metode pemainggilan tanpa baris baru.
+        /*prinln memliki fungsi hampir sama dengan print, println
+          juga digunakan untuk metode pemanggil tetapi dengan baris baru.
+        */
+        /*printf itu memiliki fungsu yg berbeda diantara 2 metode sebelumnya,
+          metode ini kita gunakan apabila kita ingin menggunakan format seperti
+          pada codingan diatas.
+        */
+}
+}
