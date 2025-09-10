1acd542ffaae04292513c64c2a6ef0ef1e4d8f75
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-11 00:01:05 +0800

    Create Day04.java

---
+++ Day04.java
@@ -0,0 +1,25 @@
+public class Abdal_100dayscoding {
+public static void main(String [] args) {
+    String nama = ("\"Ahmad Abdal Palalo\"");
+    String tempat = ("Tt\binambung le\'ba");
+    int umurAngka = 19;
+    String umur = "Sembilan belas tahun";
+    int tinggi = 170;
+    double beratbadan = 60.3;
+    
+    System.out.print("Nama\t: " + nama + "\nAlamat\t: " + tempat + "\nUsia\t: " 
+    + umurAngka + "\\" + umur + "\nTinggi\t: " + tinggi + "cm" + "\nBerat Badan: " + beratbadan + "kg");
+    
+    /*Pada Day04 ini saya membuat codingan dengan perintah "Escape Sequence", dan
+      dapat dilihat dari codingan saya hari ini saya menggunakan 5 jenis Escape Sequence
+      yaitu:
+      1. \t -> Tab
+      2. \n -> Enter/baris baru/New line. 
+      3. \b -> Backspace (untuk menghapus satu karakter sebelumnya). 
+      4. \" -> digunakan apabila ingin menambahkan tanda kutip ganda (") pada kalimat. 
+      5. \' -> digunakan apabila ingin menambahkan tanda kutip tunggal (') pada kalimat. 
+      6. \\ digunakan untuk menampilkan garis miring terbalik pada kalimat/kata.
+      */
+      
+	}
+}
