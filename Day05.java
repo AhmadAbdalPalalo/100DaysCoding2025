b08c29136037938d22c474c2a56776229c57aa46
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-12 05:48:28 +0800

    Create Day05.java

---
+++ Day05.java
@@ -0,0 +1,27 @@
+// DAY 5 Tipe data Numerik Bilangan Bulat.
+
+public class Abdal_100dayscoding {
+public static void main(String[] args) {
+
+byte usia = 19;
+        short tahun = 2006;
+        int tanggal = 30;
+        long tbt = 30092006l;
+
+        System.out.println("Usia\t: " + usia + "\nTahun\t: " + tahun + "\nTanggal\t: " + tanggal + "\nTBT\t: " + tbt);
+
+  
+        /* -byte -> byte menyimpan nilai bilangan bulat kecil (8 bit, -128 hingga 127).
+        
+           -short -> short menyimpan nilai yang sedikit lebih besar
+                     (16 bit, -32.768 hingga 32.767).
+        
+           -integer -> int adalah yang paling sering digunakan
+                       (32 bit, sekitar -2 miliar hingga 2 miliar).
+        
+           -long -> long digunakan untuk bilangan bulat yang sangat besar (64 bit).
+                    jika ingin menggunakan tipe data ini, kita di haruskan untuk menggunakan
+                    huruf l diakhir anggka yg kita input.
+        */
+}
+}
