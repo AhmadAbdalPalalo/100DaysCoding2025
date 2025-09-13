af31eda2f3a826883df37f25645f06df9e42d8bf
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-14 00:00:27 +0800

    Create Day07.java

---
+++ Day07.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+      char kelamin = 'L';
+      boolean rill = true;
+      
+      System.out.println("Gender\t: "+kelamin);
+        System.out.println("Fans King Emyu: "+rill);
+
+        /* - char -> tipe data primitif yang digunakan untuk menyimpan satu karakter tunggal,
+             seperti huruf, angka, simbol, atau spasi, dalam program komputer dan diapit oleh tanda kutip tunggal
+             (misalnya, 'A').
+           - boolean -> boolean adalah tipe data logika yang hanya memiliki
+             dua nilai: benar (true) atau salah (false).
+            */
+    }
+}
