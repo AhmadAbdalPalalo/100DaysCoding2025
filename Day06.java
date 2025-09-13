d049d2cacc2c55f9ffd4c28d9d5a6e77dd502cc9
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-13 17:21:38 +0800

    Create Day06.java

---
+++ Day06.java
@@ -0,0 +1,17 @@
+package Abdal_100dayscoding;
+
+public class Abdal_100dayscoding {
+public static void main(String[] args) {
+   double tinggi = 170.1;
+      float bb = 60.5f;
+      System.out.println("Tinggi badan\t: " + tinggi + "\nBerat Badan\t: " + bb);
+        
+        /* - double -> Tipe data ini digunakan apabila kita ingin menginput angka desimal/berkoma (,),
+                       namun tipe data ini memiliki batas maksimal angka di belankang koma yaitu 
+                       sebanyak 15 digit.
+    
+           - float -> Sama seperti dengan double, namun tipe data float memiliki 
+                      batas angkat hanya 7 digit dibelakang koma dan di haruskan menambahkan huruf "f" diakhir angka.
+          */
+  }
+}
