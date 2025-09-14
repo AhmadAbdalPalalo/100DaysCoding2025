5813ebb8d4f9b38d595f55ecd2959f531e92e967
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-15 00:32:03 +0800

    Create Day08.java

---
+++ Day08.java
@@ -0,0 +1,12 @@
+// DAY 8 Pengenalan tipe data String
+
+public class Main {
+    public static void main(String[] args) {
+      String keterangan = "Nama saya Ahmad Abdal Palalo, saya tinggal di Tinambung,\ndan saya adalah mahasiswa di Universitas Sulawesi Barat.";
+        System.out.println(keterangan);
+
+        /* - String -> tipe data yg daot menginput serangkaian karakter, seperti huruf, angka, simbol,
+                       juga spasi, dan harus diapit dngan tanda kutip dua (").
+        */
+  }
+}
