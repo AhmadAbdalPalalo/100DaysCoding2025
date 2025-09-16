bf9ac633fd080dcd9a507f8a277597f0ec9006ab
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-16 16:52:50 +0800

    Create Day09.java

---
+++ Day09.java
@@ -0,0 +1,27 @@
+      // DAY 9
+
+public class Main {
+
+    public static void main(String[] args) {
+      //Deklarasi Variable
+        String  nama;
+        int umur;
+        byte anak;
+        double tinggi;
+        
+     //Inisialisasi Variable
+        nama = "Abdal Palalo";
+        umur = 18;
+        anak = 1;
+        tinggi = 170.1;
+        
+      //Update Variable
+        nama = "Abdal Ganteng";
+        umur = 32;
+        anak = 3;
+        tinggi = 170.9;
+        
+        System.out.println("Namaku: "+nama);
+        System.out.println("Umurku: "+umur);
+        System.out.println("Anak ke: "+anak);
+        System.out.println("Tinggiku: "+tinggi);
