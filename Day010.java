fda53e3055876a330509ae417d02f8222756a4a8
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-17 16:51:25 +0800

    Create Day010.java

---
+++ Day010.java
@@ -0,0 +1,12 @@
+//DAY 10 Constanta (Final variabel)
+
+public class Main {
+    public static void main(String[] args) {
+
+        String nama = "Abdal";
+        final int umur = 18;
+        final double tinggi = 170.3;
+        
+        System.out.println("Nama: "+nama);        
+        System.out.println("Umur: "+umur); 
+        System.out.println("Tinggi: "+tinggi);  
