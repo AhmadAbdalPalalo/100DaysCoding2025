cae8f98a5a4cf38dcc151b631184bffa65240d6b
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-18 15:49:28 +0800

    Create Day011.java

---
+++ Day011.java
@@ -0,0 +1,15 @@
+// Day 011 Pengenalan Scanner
+
+input java.util.Scanner
+
+public class Main {
+    public static void main(String[] args) {
+
+    Scanner A = new Scanner (System.in);
+    System.out.print("Klub terbaik di dunia??? ");
+    String a = A.nextLine();
+
+    System.out.println(a);
+
+  }
+}
