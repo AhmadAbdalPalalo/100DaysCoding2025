d5f5d84c84e18a94704caf77fcb8ece46f49ef7a
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-18 19:05:24 +0800

    Update Day011.java

--- Day011.java
+++ Day011.java
@@ -1,6 +1,6 @@
 // Day 011 Pengenalan Scanner
 
-input java.util.Scanner
+impor java.util.Scanner
 
 public class Main {
     public static void main(String[] args) {
@@ -8,8 +8,11 @@ public class Main {
     Scanner A = new Scanner (System.in);
     System.out.print("Klub terbaik di dunia??? ");
     String a = A.nextLine();
-
     System.out.println(a);
+    A.close();
 
+    /* Scanner digunakan apabila kita ingin menambahkan output-an dengan cara di ketik langsung, jika menggukan Scanner kita
+        harus meanmbahkan kelasnya yaitu java.util.Scanner dengan pernyataan "import java.util.Scanner;".
+        */
   }
 }
