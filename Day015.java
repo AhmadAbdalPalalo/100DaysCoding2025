5ae667f750b0aa5e8418214d6d8a8c0243628ac9
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-22 20:08:43 +0800

    Create Day015.java

---
+++ Day015.java
@@ -0,0 +1,13 @@
+// DAY 015 Aritmatika Penjumlahan (+) & Pengurangan (-).
+public class Main {
+    public static void main(String[] args) {
+          
+        byte bil1 = 21;
+        byte bil2 = 21;
+        System.out.println("21 + 21 = " + (bil1 + bil2));
+        
+        byte bil3 = 42;
+        byte bil4 = 21;
+        System.out.println("42 - 21 = " + (bil3 - bil4));
+    }
+}
