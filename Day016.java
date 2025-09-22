d670321e4cb2368bb8a115ec09279acbedc8dbe0
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-23 07:29:22 +0800

    Create Day016.java

---
+++ Day016.java
@@ -0,0 +1,12 @@
+// DAY 016 Aritmatika Perkalian (*) & Pembagian (/).
+public class Main {
+    public static void main(String[] args) {
+      int bil1 = 21;
+        int bil2 = 21;
+        System.out.println("21 x 21 = " + (bil1 * bil2));
+        
+        int bil3 = 42;
+        int bil4 = 2;
+        System.out.println("42 / 2 = " + (bil3/bil4));
+    }
+}
