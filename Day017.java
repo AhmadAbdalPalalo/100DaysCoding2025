a7bf8c1aa27eedcf64c20a424cc5f3698daeda9b
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-24 00:00:28 +0800

    Create Day017.java

---
+++ Day017.java
@@ -0,0 +1,10 @@
+// Day 017 Aritmatika Modulus/sisa bagi (%) 
+
+public class Main {
+	public static void main(String[] args) {
+		
+		int bil1 = 5;
+		int bil2 = 2;
+		System.out.println("5 % 2 = " + (bil1%bil2));
+	}
+}
