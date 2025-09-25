cf30bc1e265f02c692949a60927390e037c4e73e
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-25 13:23:58 +0800

    Create Day018.java

---
+++ Day018.java
@@ -0,0 +1,10 @@
+// DAY 018 Konversi otomatis (tipe data kecil ke besar).
+
+public class Abdalllll {
+    public static void main(String[] args) {
+        byte tipe = 21;
+        short data = tipe;
+        System.out.println(data);
+
+    }
+}
