ad8e68ffe8527227bca2eec48d82871d833fafe7
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-26 19:34:22 +0800

    Create Day019.java

---
+++ Day019.java
@@ -0,0 +1,9 @@
+// DAY 019 Konversi Manual/paksa (dari tipe data besar ke kecil).
+public class Abdal_Ganteng {
+    public static void main(String[]args){
+     
+        short besar = 21;
+        byte keKecil = (byte) besar;
+        System.out.println(keKecil);
+}
+}
