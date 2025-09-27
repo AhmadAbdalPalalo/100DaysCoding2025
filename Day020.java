61b870399e0f57286e0bcf31fab8c0bca85f8edb
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-27 20:22:36 +0800

    Create Day020.java

---
+++ Day020.java
@@ -0,0 +1,17 @@
+// DAY 020 Mengubah Tipe data primitif ke String.
+public class Abdalllll {
+    public static void main(String[] args) {
+        
+        int a = 21;
+        String b = Integer.toString(a);
+        System.out.println(b);
+
+        /* Di day 20 ini kita membuat codingan 'konversi tipe data primitif ke string, untuk mengubah tipe data primitif ke dtring itu ada dua motede, yaitu
+          dengan perintah String.valueOf(); dan .toString(); ,,, Pada codingan sya kali ini sya menggunakan perintah .toString();.
+          Dalama menggunakan perintah .toString(); ini kita harus mengetikan terlebih dahulu tipe data primitif yg ingin kita ubah ke String dan menuliskan tipe data 
+          tersebut dengan huruf kapital di awal Tipe data, contohnya jika sya ingin mengubah dari tipe data double ke String kita mengetikkan "Double.toString();" seperti ini
+          dan begitupula pada tipe data primitf lainnya jika ingin di konversi ke string.                                                       
+        */
+      
+    }
+}
