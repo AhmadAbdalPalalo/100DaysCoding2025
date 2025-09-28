801b3384f94d439ceac716f897da0c4bbc1b55f5
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-28 14:32:41 +0800

    Create Day021.java

---
+++ Day021.java
@@ -0,0 +1,22 @@
+// DAY 021 Latihan: Program untuk menukar nilai dua variable.
+public class AbdalGanteng {
+public static void main (String [] args) {
+    
+        int a = 100;
+        int b = 10;
+        System.out.println("nilai a sebelum ditukar: "+a);
+        System.out.println("nilai b sebelum ditukar: "+b);
+
+        a = a+b; //110
+        b = a-b; //100
+        a = a-b; //10 
+
+        System.out.println("nilai a setelah ditukar: "+a);
+        System.out.println("nilai b setelah ditukar: "+b);
+
+  /* jadi di Day 21 satu ini kita membuat codingan penukaran nilai dari kedua variable dengan hanya menggunakan 2 variabel,
+    kita melakukan  penukaran dengan variabel yang sudah ada yaitu, a dan b tanpa ada variable tanbahan. cara penukarannya itu dengan menggunakan
+    metode aritmatika
+  */
+  }
+}
