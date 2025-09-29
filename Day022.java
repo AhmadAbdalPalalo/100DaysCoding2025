d8c9fcf03bb19430a52b14553b83f1c71b4d948a
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-09-29 17:51:51 +0800

    Create Day022.java

---
+++ Day022.java
@@ -0,0 +1,13 @@
+    // DAY 022 Latihan: Program luas persegi.
+
+import java.util.Scanner;
+public class Abdalllll {
+    public static void main(String[] args) {
+        Scanner D = new Scanner (System.in);
+         System.out.print("Masukkan sisi: ");
+         int sisi = D.nextInt();
+         
+         long keliling = sisi*sisi;
+         System.out.println("Keliling persegi: " + keliling);
+    }
+}
