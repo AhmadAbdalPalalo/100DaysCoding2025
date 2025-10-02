bda715d8e4e38bf60f97daefc3cde823b8822286
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-10-02 20:09:31 +0800

    Create Day025.java

---
+++ Day025.java
@@ -0,0 +1,17 @@
+// DAY 025 Increment & Decrement
+
+public class AbdalBingung {
+    public static void main(String[] args) {
+
+        int a = 21;
+        int b = 21;
+        System.out.println("Nilai A SEBELUM Increment: " + a);
+        System.out.println("Nilai B SEBELUM Decrement: " + b);
+
+         a++; //Peintah INCREMENT, otomatis nilai dari variable a jdi bertambah 1. yg tadinya 21 berubah menjadi 22
+         b--; //Perintah DECREMENT, otomatis nilai dari variable b jdi berkurang 1. yg tadinya 21 berubah menjadi 20
+
+        System.out.println("Nilai A SETELAH Increment: " + a);
+        System.out.println("Nilai B SETELAH Decrement" + b);
+    }
+}
