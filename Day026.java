b89a3c01f694648634442c7ca00c9be6281e1904
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-10-03 19:56:22 +0800

    Create Day026.java

---
+++ Day026.java
@@ -0,0 +1,25 @@
+// DAY 026 Penugasan (=, +=, -=, dll).
+
+public calss Abdallagigalauuuu {
+public static void main (String [] args) {
+
+        int nilai = 21;
+        System.out.println("Nilai awal : " + nilai);
+        
+        nilai = 40; //Penugasan/sama dengan di ubah nilai dari variabel nilai/di update
+        System.out.println("Nilai  (=) : " + nilai);
+        
+        nilai += 20; //Penambhana variabel nilai
+        System.out.println("Nilai (+=) : " + nilai);
+        
+        nilai -= 20; //Pengurangan variabel nilai
+        System.out.println("Nilai (-=) : " + nilai);
+        
+        nilai /= 2; //Pembagian variabel nilai
+        System.out.println("Nilai (/=) : " + nilai);
+        
+        nilai %= 3; //Pem-Modulusan variabel nilai
+        System.out.println("Nilai (%=) : " + nilai);
+        
+    }
+}
