44b37449154f03e705e9d34395291835557da258
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-10-04 23:14:38 +0800

    Create Day027.java

---
+++ Day027.java
@@ -0,0 +1,20 @@
+// SOAL Evaluasi
+
+import java.util.Scanner;
+public class H_min1_ICL {
+    public static void main (String [] args){
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.print("Nama: ");
+        String nama = sc.nextLine();
+        
+        System.out.print("Umur: ");
+        int umur = sc.nextByte();
+        
+        int limatahun = umur + 5;
+        System.out.println("Halo, nama saya " + nama + ".");
+        System.out.print("Umur saya " + umur + " tahun.\n");
+        System.out.printf("Dalam 5 tahun lagi umur saya %d tahun.",tahun);
+       
+  }
+}
