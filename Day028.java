64e93eaf9c698f320a3d906b03596eae48841ff9
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-10-05 20:10:33 +0800

    Create Day028.java

---
+++ Day028.java
@@ -0,0 +1,51 @@
+public class abdalggkatapengawasevaluasi {
+  public static void main (String [] args) {
+           System.out.println("= = Operator perbandingan (==) = =");
+        int a = 20;
+        int b = 20;
+        System.out.printf("Variable A = %d\nVariable B = %d \n",a,b);
+        System.out.println("Apakah A dan B SAMA? " + (a == b));
+        System.out.println("==========================");
+        
+        int c = 21;
+        int d = 20;
+        System.out.printf("Variable C = %d\nVariable D = %d \n",c,d);
+        System.out.println("Apakah C dan D SAMA? " + (c == d));
+        
+        System.out.println("\n= = Operator perbandingan (!=) = =");
+        int e = 20;
+        int f = 20;
+        System.out.printf("Variable E = %d\nVariable F = %d \n",e,f);
+        System.out.println("Apakah E dan F BERBEDA? " + (e == f));
+        System.out.println("==========================");
+        
+        int g = 21;
+        int h = 20;
+        System.out.printf("Variable G = %d\nVariable H = %d \n",g,h);
+        System.out.println("Apakah G dan H BERBEDA? " + (g == h));
+        
+        
+        /* -> "(==) / SAMA DENGAN" 
+              Merupakan metode perbandingan yang menanyakan APAKAH DATA 1 DAN 2 SAMA?
+              apabila kedua data tersebut "sama", maka program akan menjawab "true" karena kedua
+              data tersebut sama. contohnya dapat dilihat pada perbandingan variable a dan b.
+              Begitupun sebaliknya, apabila kita membandingkan 2 data yang "berbeda" otomatis 
+              program akan menjawab "false" karena kedua data tersebut berbeda. contohnya dapat
+              dilihat pada perbandingan variavle c dan d.
+              
+           -> "(!=) / TIDAK SAMA DENGAN" 
+              Merupakan metode perbandingan yg dimana program menanyakan APAKAH DATA 1 DAN 2
+              BERBEDA?"... Ini kebalikan dari perbandingan (==)/SAMA DENGAN, di metode ini
+              apabila kedua data yg dibandingkan itu "berbeda" maka program akan menjawab "true"
+              karena kedua data yg dibandingkan itu berbeda. Contohnya dapat dilihat pada perbandingan
+              variable e dan f.
+              Begitupun sebaliknya, apabila data yang dibandingan itu "sama" maka program akan
+              menjawab "false" karena data yg dibandingkan itu "sama". Contohnya dapat dilihat
+              pada perbandingan variable g dan h.
+        
+            Intinya pada metode ini itu adalah perbandingan yang membandingkan data 1 dan data 2.
+            CMIW...hehhe
+        */
+    }
+    
+}
