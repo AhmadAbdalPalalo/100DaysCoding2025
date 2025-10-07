8dd94f19adeed82f725514f74d5200d26c1eb3ff
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-10-07 19:58:50 +0800

    Create Day030.java

---
+++ Day030.java
@@ -0,0 +1,10 @@
+// DAY 030 Operator Perbandingan (<= dan >=)
+
+public class AbdalUdahNyampeDayTigaPuluh {
+  public static void main (String [] args) {
+    int a = 21;
+    int b = 12;
+    System.out.println("Apakah A lebih KECIL/SAMA dngn B? " + (a <= b)); //Perbandingan "lebih kecil atau sama dengan"
+    System.out.println("Apakah A lebih BESAR/SAMA dngn B? " + (a >= b)); //Perbandingan "lebih besar atau sama dengan"
+  }
+}
