3b478ccb22f57793ad7586026337891fc3796197
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-11-14 22:56:27 +0800

    Create Day068.java

---
+++ Day068.java
@@ -0,0 +1,12 @@
+// Day 068 Membuat Pola Persegi.
+
+public class Day068_Membuat_Pola_Persegi {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 7; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
