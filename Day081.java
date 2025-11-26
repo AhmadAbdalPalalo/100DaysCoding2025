451d470b0179ad87102357d72d78bca58decd3aa
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-11-27 00:29:04 +0800

    Create Day081.java

---
+++ Day081.java
@@ -0,0 +1,10 @@
+// Day 081 Mengakses Array.
+
+public class Day081_Mengakses_Array{
+	public static void main(String[] args) {
+		int abdalGanteng [] = {14,28,21};
+		System.out.println(abdalGanteng[0]);
+		System.out.println(abdalGanteng[1]);
+		System.out.println(abdalGanteng[2]);
+	}
+}
