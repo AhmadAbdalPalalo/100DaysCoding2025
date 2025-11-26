b16a61edf2229a665ead63baa8ac128739537175
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-11-26 22:44:35 +0800

    Create Day080.java

---
+++ Day080.java
@@ -0,0 +1,11 @@
+//Day  056 Perulangan do-while.
+
+public class Day080_Array_1D {
+    public static void main(String[] args) {
+        
+        int a [] = {1,2,3,4,5};
+        for (int b : a) {
+            System.out.print(b + " ");
+        }
+    }
+}
