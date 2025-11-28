8850275ebdb153adc3d072881d314648942f56fc
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-11-28 21:22:56 +0800

    Create Day082.java

---
+++ Day082.java
@@ -0,0 +1,10 @@
+// Day 082
+
+public class Day_82_ {
+    public static void main(String[] args) {
+        int a[] = {21,31,28};
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
