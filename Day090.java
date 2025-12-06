268f7592d320fae2eff4fa00624a6e6278e0298e
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-12-06 23:22:26 +0800

    Create Day090.java

---
+++ Day090.java
@@ -0,0 +1,8 @@
+public class abdal_siap_di_kick_dari_ISC {
+public static void main(String[] args) {
+    System.out.println(luas(10,5));
+}
+    public static int luas(int panjang, int lebar){
+        return panjang * lebar;
+    }
+}
