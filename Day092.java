ae91b53c3eb47cb831c36c53198da1cf40501c2c
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-12-08 23:07:15 +0800

    Create Day092.java

---
+++ Day092.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(jumlah(10, 5));
+    }
+    static int jumlah(int a, int b){
+        return a * b;
+    }
+}
