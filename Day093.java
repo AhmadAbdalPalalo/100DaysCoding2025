4c5cd68e42c9595d9f40bc5e50121ad4e4a60819
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-12-09 20:11:49 +0800

    Create Day093.java

---
+++ Day093.java
@@ -0,0 +1,13 @@
+// Day 093 method ceil(), floor(), round()
+
+import java.util.Scanner;
+
+public class Day093 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double angka = in.nextDouble();
+        System.out.println(Math.ceil(angka));
+        System.out.println(Math.floor(angka));
+        System.out.println(Math.round(angka));
+    }
+}
