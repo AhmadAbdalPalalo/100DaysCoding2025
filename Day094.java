40705be4ba7a79ec6808ea2771d72a70b934fc19
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-12-10 20:19:07 +0800

    Create Day094.java

---
+++ Day094.java
@@ -0,0 +1,12 @@
+// Day 094_Memanggil dengan method void(maybe)
+
+public class Day094 {
+    public static void main(String[] args) {
+        cetakAngka();
+    }
+    static void cetakAngka() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i + " ");
+        }
+    }
+}
