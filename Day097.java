6a55070ac358b7d6beb78d24420efcb87684c74b
AhmadAbdalPalalo <abdalpalalo.belajar@gmail.com>
2025-12-13 07:22:57 +0800

    Create Day097.java

---
+++ Day097.java
@@ -0,0 +1,9 @@
+public class Day097 {
+    public static void main(String[] args) {
+        System.out.println(LuasPersegi(10,5));
+    }
+    static int LuasPersegi(int p, int l){
+      System.out.print("Luas persegi = ") 
+        return p * l;
+    }
+}
